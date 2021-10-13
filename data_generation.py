6d3d9c8d3acd955d1cf7ae2b899bfe8dd74697be
Martin <martinfanev01@gmail.com>
2021-10-13 19:53:43 +0100

    bug fix + visualisation notebook

--- data_generation.py
+++ data_generation.py
@@ -3,21 +3,32 @@ import numpy as np
 
 class simple_generation:
 
-    def __init__(self, no_samples):
+    def __init__(self):
+        pass
         
-        self.no_samples = no_samples
-        
-    def gaussian_fit(self, mean, std):
+    def gaussian_fit(self, mean, std, no_samples):
         """
         Returns array corresponding to a gaussian distribution.
-        """
-        gauss_dist = np.random.normal(mean, std, self.no_simples)
         
-        return gaussian_dist
+        Arguments:
+        
+        - mean: the mean of the distribution
+        - std: standard deviation of the distribution
+        - no_samples: number of generated samples
+        """
+        gauss_dist = np.random.normal(mean, std, no_samples)
+        
+        return gauss_dist
     
     def linear_fit(self, start_point, step_size, bins):
         """
         Returns array corresponding to a linear distribution.
+        
+        Arguments: 
+        
+        - start_point: the starting point of the distribution.
+        - step_size: the step size when changing x.
+        - bins: the amount of bins on the histogram
         """
         dist = []
         function = lambda x: x
@@ -26,7 +37,7 @@ class simple_generation:
             n_points = int(function(start_point))
             x_1 = start_point
             x_2 = step_size + start_point
-            bin_ = np.random.uniform(x_1, x_2, n_points)
+            bin_ = np.random.uniform(x_1, x_2, abs(n_points))
             start_point += step_size
             dist.append(bin_)
 
@@ -34,17 +45,27 @@ class simple_generation:
     
         return dist_conc
 
-    def exp_fit(self):
+    def exp_fit(self, no_samples):
         """
         Returns array corresponding to a exponential distribution.
+        
+        Arguments:
+        
+        - no_samples: number of samples.
         """
-        exp_dist = np.random.exponential(1,self.no_samples)
+        exp_dist = np.random.exponential(1, no_samples)
         
         return exp_dist
 
     def quad_fit(self, start_point, step_size, bins):
         """
         Returns array corresponding to a exponential distribution.
+        
+        Arguments:
+        
+        - start_point: the starting point of the distribution.
+        - step_size: the step size when changing x.
+        - bins: the amount of bins on the histogram
         """
         dist = []
         function = lambda x: x**2
