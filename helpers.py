9a452ae3df9d9b440e78086d6108a4f6ad60feab
Sergio20f <sergio.calvo.cs@gmail.com>
2021-12-20 02:21:11 +0100

    Add more helpers functions and summarise statistics of training data and generated data distributions

--- helpers.py
+++ helpers.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 from celluloid import Camera
 
 
@@ -41,7 +42,7 @@ def show_samples(generated_point_list, epoch, generator, data, n=100, l_dim=5):
         generated_point_list.append(generated_data)
 
 
-def make_animation(real_data:tuple, generated_point_list):
+def make_animation(real_data: tuple, generated_point_list):
     """
     Function that makes an animation of the generated data. It can be used to evaluate how the generator is doing with
     respect to time or epochs.
@@ -54,8 +55,8 @@ def make_animation(real_data:tuple, generated_point_list):
 
     camera = Camera(plt.figure())
 
-    plt.xlim(real_x.min()-0.2, real_x.max()+0.2)
-    plt.ylim(real_y.min()-0.05, real_y.max()+0.05)
+    plt.xlim(real_x.min() - 0.2, real_x.max() + 0.2)
+    plt.ylim(real_y.min() - 0.05, real_y.max() + 0.05)
 
     for i in range(len(generated_point_list)):
         plt.scatter(real_x, real_y, c='blue')
@@ -66,3 +67,59 @@ def make_animation(real_data:tuple, generated_point_list):
     animation = camera.animate(blit=True)
     plt.close()
     animation.save('animation.gif', fps=10)
+
+
+def stats_dist(dist, dist2=None, prnt=True):
+    """
+    Calculates the mean, standard deviation, variance, and covariance matrix of a distribution. The function also includes
+    the option to calculate the covariance matrix of two distributions. To do this, pass the second distribution as the
+    second parameter.
+
+    Parameters:
+        - dist (numpy array): The distribution to be analyzed.
+        - dist2 (numpy array): The second distribution to be analyzed.
+        - prnt (bool): Whether or not to print the results. If print is False, the function will simply return the
+        results.
+    """
+    if prnt:
+        print("-------------------------------")
+        print("Mean:", np.mean(dist))
+        print("-------------------------------")
+        print("Standard deviation:", np.std(dist))
+        print("-------------------------------")
+        print("Variance:", np.var(dist))
+        print("-------------------------------")
+        if dist2 is not None:
+            print("Covariance matrix:", np.cov(dist, dist2))
+            return np.mean(dist), np.std(dist), np.var(dist), np.cov(dist, dist2)
+        else:
+            print("Covariance:", np.cov(dist))  # Will be a scalar
+
+    return np.mean(dist), np.std(dist), np.var(dist), np.cov(dist)
+
+
+def df_style(_):
+    return "font-weight: bold"
+
+
+def df_generator(stats_or_x, stats_gen_x, stats_or_y, stats_gen_y, rows: np.ndarray, df_style):
+    """
+    Generates a dataframe with the given parameters.
+
+    Parameters:
+        - stats_or_x: The stats of the x-dimension from the train data.
+        - stats_gen_x: The stats of the x-dimension from the generated data.
+        - stats_or_y: The stats of the y-dimension from the train data.
+        - stats_gen_y: The stats of the y-dimension from the generated data.
+        - rows: Numpy array with the rows of the dataframe as strings.
+        - df_style: The style of the dataframe. Should be coming from a function like "df_style"
+    """
+    stats_df = pd.DataFrame()
+    stats_df[" "] = rows
+    stats_df["original_x_dim"] = np.array([stats_or_x[0], stats_or_x[1], stats_or_x[2], stats_or_x[3].item()])
+    stats_df["generated_x_dim"] = np.array([stats_gen_x[0], stats_gen_x[1], stats_gen_x[2], stats_gen_x[3].item()])
+    stats_df["original_y_dim"] = np.array([stats_or_y[0], stats_or_y[1], stats_or_y[2], stats_or_y[3].item()])
+    stats_df["generated_y_dim"] = np.array([stats_gen_y[0], stats_gen_y[1], stats_gen_y[2], stats_gen_y[3].item()])
+    stats_df = stats_df.style.applymap(df_style, subset=[" "])
+
+    return stats_df
